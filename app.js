3cd8688c703bed690e4d599a72125945b806808e
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-05-25 09:13:49 +0500

    image swaping

---
+++ app.js
@@ -0,0 +1,51 @@
+// function submit(){
+//     var cities = document.getElementsByName("city");
+//      // yeh ciities aik array hai
+//     for(var city of cities){
+//          //for of loop only array pr chlta hai
+//         if(city.checked){
+//             console.log(city.value);    
+//         }
+//     }
+// }
+
+// function submition(){
+//     var cities = document.getElementsByName("sheher");
+//     // yeh ciities aik array hai
+//    for(var city of cities){
+//         //for of loop only array pr chlta hai
+//        if(city.checked){
+//            console.log(city.value);    
+//        }
+//    }
+// }
+
+// function upload(){
+//     var file = document.getElementById("file");
+//     var image = document.getElementById("image");
+//     image.src = URL.createObjectURL(file.files[0]); // temporary opject ka url bnaddeta hai 
+//                                      //array ki form me value store hokr aati hai
+//     image.style.display = "block";
+// }
+
+// function submitted(get){
+//     console.log(get)
+//     if(get == "over"){
+// event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoWcVqK8e_jAPt7mgQ9hMPANlQeFZUAeoJpQ&s"
+//     }
+// else {
+//     event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfZlTkB19LynsbzV3g5ZioziaKTCi9VwAYIE8xrg_0hSxI3i0wiNEPgaLGIPVHh2rJWHo&usqp=CAU"
+// }
+// }
+
+
+// final version of swaping image
+
+// function submitted(event){
+//     if(event.type == "mouseover"){
+// event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoWcVqK8e_jAPt7mgQ9hMPANlQeFZUAeoJpQ&s"
+// }
+// else {
+//     event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfZlTkB19LynsbzV3g5ZioziaKTCi9VwAYIE8xrg_0hSxI3i0wiNEPgaLGIPVHh2rJWHo&usqp=CAU"
+// }
+// }
